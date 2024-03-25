930f02b32fdd33312a33ab6f8f93c12dcf937417
ChristopherVuvu <vuvumumo93@gmail.com>
2024-03-25 16:30:23 +0300

    added routes to App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+// src/App.js
+import React, { useState } from 'react';
+import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
+import Login from './components/Login';
+import Sales from './components/Sales';
+import Products from './components/Products';
 
-function App() {
+const App = () => {
+  const [loggedIn, setLoggedIn] = useState(false);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <Switch>
+          <Route path="/login">
+            <Login setLoggedIn={setLoggedIn} />
+          </Route>
+          <Route path="/sales">
+            {loggedIn ? <Sales /> : <Redirect to="/login" />}
+          </Route>
+          <Route path="/products">
+            {loggedIn ? <Products /> : <Redirect to="/login" />}
+          </Route>
+          <Redirect to="/login" />
+        </Switch>
+      </div>
+    </Router>
   );
-}
+};
 
 export default App;
+
